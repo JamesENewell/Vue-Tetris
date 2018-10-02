56c59b5734d72f9c6de93c7f327df77076e8e9b1
dcravenor <happy-james@hotmail.co.uk>
2018-10-02 11:01:02 +0100

    added test shape and coordinates

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 $(()=>{
   console.log('Hello There - General Kenobi');
 
+  var testShapeCoordinates = [{row: 1, col: 1},{row: 2, col: 1},{row: 1, col: 2},{row: 2, col: 2}];
 
   function drawBoard() {
     for(var row=0;row<22;row++){
@@ -11,6 +12,17 @@ $(()=>{
     }
   }
 
+  function fillShape(coordinates,fillColor) {
+    for(var i=0;i<coordinates.length;i++){
+      var row = coordinates[i].row;
+      var col = coordinates[i].col;
+      var $coor = $('.'+row).find('#'+col);
+      $coor.attr('bgcolor',fillColor);
+    }
+  }
+
+
   drawBoard();
+  fillShape(testShapeCoordinates,'blue');
 
 });
