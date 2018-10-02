1ac148a83dafe981326f5d95b3eff7106a8c8945
dcravenor <happy-james@hotmail.co.uk>
2018-10-02 10:25:00 +0100

    changed drawBoard function to reflect table use

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
 $(()=>{
   console.log('Hello There - General Kenobi');
 
-  
+
+  function drawBoard() {
+    for(var row=0;row<22;row++){
+      $('#gameBoard').append('<tr class="'+row+'"></tr>');
+      for (var col=0;col<10;col++){
+        $('.'+row).append('<td id="'+col+'"></td>');
+      }
+    }
+  }
+
+  drawBoard();
+
 });
